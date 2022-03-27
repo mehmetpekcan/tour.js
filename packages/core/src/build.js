754f7a9ca2a1c9a4393f98ff0bb358fdc43add79
mehmetpekcan <pekcanmehmett@gmail.com>
2022-03-27 16:37:24 +0300

    Add yarn-workspace as a monorepo structure

---
+++ packages/core/src/build.js
@@ -0,0 +1,27 @@
+const args = process.argv.slice(2);
+
+const parseArgs = (args) =>
+  Object.fromEntries(args.map((arg) => arg.split("=")));
+
+const { watch = false } = parseArgs(args);
+
+require("esbuild")
+  .build({
+    entryPoints: ["src/index.js"],
+    outdir: "dist",
+    bundle: true,
+    sourcemap: true,
+    minify: true,
+    splitting: true,
+    format: "esm",
+    target: ["esnext"],
+    watch: Boolean(watch),
+  })
+  .then((result) => {
+    if (Boolean(watch)) {
+      console.log("Watching for your changes...");
+    } else {
+      console.log("Build done...");
+    }
+  })
+  .catch(() => process.exit(1));
