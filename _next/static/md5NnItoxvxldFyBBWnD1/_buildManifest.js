a0d8f5f7d95aa500a7721966c34115e9a6e04a6f
mehmetpekcan <60665510+mehmetpekcan@users.noreply.github.com>
2022-04-25 22:04:56 +0000

    Deploying to gh-pages from @ mehmetpekcan/tour.js@26f0f289385a6f23cada73acd79c0b0548da9fb8 🚀

--- _next/static/md5NnItoxvxldFyBBWnD1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-94fb8a4adb9224be.js"],"/_error":["static/chunks/pages/_error-5f79c00932c7c9f1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
