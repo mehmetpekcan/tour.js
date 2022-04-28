f86492f461ab8215709829bb87ed5eb767efb94b
mehmetpekcan <pekcanmehmett@gmail.com>
2022-04-29 01:10:10 +0300

    Make drawer style better with drawer component of antd

--- packages/extension/src/components/Drawer/Drawer.js
+++ packages/extension/src/components/Drawer/Drawer.js
@@ -1,4 +1,4 @@
-import { Typography, Button, Row, Col } from "antd";
+import { Typography, Button, Empty, Drawer as AntdDrawer } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import { createPortal } from "react-dom";
 import { useState, useEffect, useRef } from "react";
@@ -14,9 +14,43 @@ const captureOnce = { once: true };
 const FRAMEWORK_ROOTS = ["root", "_next"];
 const HTML_ROOTS = ["HTML", "BODY"];
 
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
 
 const getElementMeta = (element) => element.getBoundingClientRect();
+
+const demoSteps = [
+  {
+    selector: ".box-1",
+    title: "First Title 🚀",
+    content:
+      "Lorem Ipsum is simply dummy Paragraph of the printing and typesetting industry.",
+    next: "Custom next ",
+  },
+  {
+    selector: ".box-2",
+    title: "Second Title 🔥",
+    content:
+      "Lorem Ipsum has been the industry's standard dummy Paragraph ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy Paragraph of the printing and typesetting industry. Lorem Ipsum is simply dummy Paragraph of the printing and typesetting industry.",
+    next: "Custom next ",
+    prev: "Custom prev",
+  },
+  {
+    selector: "h1",
+    title: "H1 Title 🔥",
+    content:
+      "Lorem Ipsum has been the industry's standard dummy Paragraph ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy Paragraph of the printing and typesetting industry. Lorem Ipsum is simply dummy Paragraph of the printing and typesetting industry.",
+    next: "Custom next ",
+    prev: "Custom prev",
+  },
+  {
+    selector: ".box-3",
+    title: "Third Title ✨",
+    content:
+      "but also the leasp into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of",
+    prev: "Custom prev",
+    finish: "Custom Finish",
+  },
+];
 
 let tour = new Tour();
 
@@ -104,36 +138,47 @@ function Drawer({ isVisible }) {
   };
 
   return (
-    <S.Drawer isVisible={isVisible}>
+    <AntdDrawer
+      visible={isVisible}
+      title="Tour.JS Crafter"
+      bodyStyle={{ padding: 0 }}
+    >
       <Container>
-        <Logo />
-      </Container>
-      <Container>
-        {/* <S.Introduction>
-        <S.Title>How to use?</S.Title>
-        <S.Text>
-          Start creating new onboarding by just one click. After clicking our
-          wizard starts listen you. You just need to hover and click the element
-          that you want to add as a walkthrough step.
-        </S.Text>
-      </S.Introduction> */}
-        <S.NewElements>
-          <Button type="dashed" icon={<PlusOutlined />} onClick={addStep}>
-            Add new tour
-          </Button>
-        </S.NewElements>
-      </Container>
-      <Container>
-        {steps.length > 0 && (
+        {steps.length > 0 ? (
           <>
+            <Title level={3}>Tours</Title>
             <List steps={steps} />
-            <button onClick={startTour}>Start Tour</button>
+            <Button type="primary" onClick={startTour}>
+              Start Tour
+            </Button>
           </>
+        ) : (
+          <Empty
+            image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+            imageStyle={{
+              height: 60,
+              display: "flex",
+              justifyContent: "center",
+            }}
+            description={
+              <S.Introduction>
+                <Title level={3}>How to use?</Title>
+                <Paragraph>
+                  Start creating new tours by clicking below button. After then,
+                  hover the element and click!
+                </Paragraph>
+              </S.Introduction>
+            }
+          >
+            <Button type="dashed" icon={<PlusOutlined />} onClick={addStep}>
+              Add new tour
+            </Button>
+          </Empty>
         )}
       </Container>
       {isHighlighterVisible &&
         createPortal(<S.Highlighter ref={highlighterRef} />, document.body)}
-    </S.Drawer>
+    </AntdDrawer>
   );
 }
 
