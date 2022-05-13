eeed0490c616736e854b93cdb8a58265fc3c315b
mehmetpekcan <pekcanmehmett@gmail.com>
2022-05-13 23:59:01 +0300

    Make dynamic fields mapping

--- packages/dashboard/src/containers/Editor/Preview/index.js
+++ packages/dashboard/src/containers/Editor/Preview/index.js
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { BiShareAlt, BiSave } from "react-icons/bi";
 
 import Title from "components/atoms/Title";
@@ -6,8 +7,75 @@ import { useEditorContext } from "../EditorProvider";
 
 import * as S from "./style";
 
+const TooltipFields = ({ fields, changeFieldValue }) => {
+  console.log(fields);
+
+  return fields.map(
+    (field) =>
+      field.condition && (
+        <field.element
+          onInput={(e) => changeFieldValue(field.name, e.target.innerText)}
+          contentEditable
+          suppressContentEditableWarning={true}
+        >
+          {field.value || (
+            <TooltipFields
+              fields={field.children}
+              changeFieldValue={changeFieldValue}
+            />
+          )}
+        </field.element>
+      )
+  );
+};
+
 function Preview({ onSave }) {
   const { draftTour, tours, changeFieldValue } = useEditorContext();
+
+  const fieldsToElements = useMemo(
+    () => [
+      {
+        condition: draftTour.title.isActive,
+        element: S.TooltipTitle,
+        name: "title",
+        value: draftTour.title.value,
+      },
+      {
+        condition: draftTour.content.isActive,
+        element: S.TooltipContent,
+        name: "content",
+        value: draftTour.content.value,
+      },
+      {
+        condition:
+          draftTour.prev.isActive ||
+          draftTour.next.isActive ||
+          draftTour.finish.isActive,
+        element: S.TooltipFooter,
+        children: [
+          {
+            condition: draftTour.prev.isActive,
+            element: S.TooltipPrevButton,
+            content: "prev",
+            value: draftTour.prev.value,
+          },
+          {
+            condition: draftTour.next.isActive,
+            element: S.TooltipNextButton,
+            content: "next",
+            value: draftTour.next.value,
+          },
+          {
+            condition: draftTour.finish.isActive,
+            element: S.TooltipFinishButton,
+            content: "finish",
+            value: draftTour.finish.value,
+          },
+        ],
+      },
+    ],
+    []
+  );
 
   return (
     <S.Preview type="transparent">
@@ -28,67 +96,10 @@ function Preview({ onSave }) {
       </S.Preview.Header>
       <S.Preview.Body>
         <S.TooltipWrapper id="tooltip-wrapper">
-          {draftTour.title.isActive && (
-            <S.TooltipTitle
-              contentEditable
-              onInput={(e) => {
-                changeFieldValue("title", e.target.innerText);
-              }}
-              suppressContentEditableWarning={true}
-            >
-              {draftTour.title.value}
-            </S.TooltipTitle>
-          )}
-          {draftTour.content.isActive && (
-            <S.TooltipContent
-              onInput={(e) => {
-                changeFieldValue("content", e.target.innerText);
-              }}
-              contentEditable
-              suppressContentEditableWarning={true}
-            >
-              {draftTour.content.value}
-            </S.TooltipContent>
-          )}
-          {(draftTour.prev.isActive ||
-            draftTour.next.isActive ||
-            draftTour.finish.isActive) && (
-            <S.TooltipFooter>
-              {draftTour.prev.isActive && (
-                <S.TooltipPrevButton
-                  onInput={(e) => {
-                    changeFieldValue("prev", e.target.innerText);
-                  }}
-                  contentEditable
-                  suppressContentEditableWarning={true}
-                >
-                  {draftTour.prev.value}
-                </S.TooltipPrevButton>
-              )}
-              {draftTour.next.isActive && (
-                <S.TooltipNextButton
-                  onInput={(e) => {
-                    changeFieldValue("next", e.target.innerText);
-                  }}
-                  contentEditable
-                  suppressContentEditableWarning={true}
-                >
-                  {draftTour.next.value}
-                </S.TooltipNextButton>
-              )}
-              {draftTour.finish.isActive && (
-                <S.TooltipFinishButton
-                  onInput={(e) => {
-                    changeFieldValue("finish", e.target.innerText);
-                  }}
-                  contentEditable
-                  suppressContentEditableWarning={true}
-                >
-                  {draftTour.finish.value}
-                </S.TooltipFinishButton>
-              )}
-            </S.TooltipFooter>
-          )}
+          <TooltipFields
+            fields={fieldsToElements}
+            changeFieldValue={changeFieldValue}
+          />
         </S.TooltipWrapper>
       </S.Preview.Body>
     </S.Preview>
