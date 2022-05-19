59cb3a0520a65714fdd44a074d6a1270b87097dc
mehmetpekcan <pekcanmehmett@gmail.com>
2022-05-19 18:34:59 +0300

    Add extension manifest

--- packages/extension/src/index.js
+++ packages/extension/src/index.js
@@ -1,10 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import "@tour.js/core";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+(function () {
+  alert("hi");
+})();
