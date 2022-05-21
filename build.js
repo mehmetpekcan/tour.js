8b635b01824c100815d27809101bafdd5952120b
mehmetpekcan <pekcanmehmett@gmail.com>
2022-05-21 16:35:17 +0300

    Handle eslint errors

--- build.js
+++ build.js
@@ -1,12 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const esbuild = require('esbuild');
 const { sassPlugin } = require('esbuild-sass-plugin');
-
-const args = process.argv.slice(2);
 
 const parseArgs = (args) =>
   Object.fromEntries(args.map((arg) => arg.split('=')));
 
-const { watch = false } = parseArgs(args);
+const { watch = false } = parseArgs(process.argv.slice(2));
 
 esbuild
   .build({
@@ -20,8 +19,8 @@ esbuild
     watch: Boolean(watch),
     plugins: [sassPlugin()],
   })
-  .then((result) => {
-    if (Boolean(watch)) {
+  .then(() => {
+    if (watch) {
       console.log('Watching for your changes...');
     } else {
       console.log('Build done...');
