e8ce01ba50658dbdd6fdd0c564f53c19d85bbdf9
mehmetpekcan <pekcanmehmett@gmail.com>
2022-05-29 12:12:13 +0300

    Add documentation layout

---
+++ docs/src/layouts/Documentation/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './style';
+
+function DocumentationLayout({ children }) {
+  return <S.DocumentationLayout>{children}</S.DocumentationLayout>;
+}
+
+export default DocumentationLayout;
